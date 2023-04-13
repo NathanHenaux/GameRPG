7e313e6a90b65d75ad023d0262661ae8250eb3e6
Nathan Henaux <59655746+NathanHenaux@users.noreply.github.com>
2023-04-13 15:18:34 +0000

    Add choose class for player

--- src/Player/playerMain.rs
+++ src/Player/playerMain.rs
@@ -1,9 +1,34 @@
+use std::io;
+use owo_colors::OwoColorize;
+
 fn main(){
     // Player choose class
+    let class = io::stdin().read_line(); 
 
-    // if Player have choose Warrior,
-    baseStats {
-        hp: 100,
-        hpMax: 100,
+    if class == "Warrior" {
+        // Create Warrior
+        player = Player::new("Warrior", "Player");
+        warriorBaseStats = player.warriorBaseStats();
+    } 
+    
+    else if class == "Mage" {
+        // Create Mage
+        player = Player::new("Mage", "Player");
+        mageBaseStats = player.mageBaseStats();
+    } 
+    
+    else if class == "Hunter" {
+        // Create Rogue
+        player = Player::new("Hunter", "Player");
+        hunterBaseStats = player.hunterBaseStats();
+    } else {
+        println!("Invalid class");
     }
+
+    // Player choose name
+    let name = io::stdin().read_line();
+
+    // Create Player with class
+    let player = Player::new(class, name);
+    return(player)
 }
--- src/Player/playerStats.rs
+++ src/Player/playerStats.rs
@@ -18,21 +18,59 @@
 
 pub struct baseStats {
     hp: u16,
-    hpMax: u16,
+    hp_max: u16,
     strength: u16,
     armor: u16,
 
     mana: u16,      // Mage
-    manaMax: u16,
+    mana_max: u16,
     intelligence: u16,
     dodging: u8,
 
-    rage: u16,      // Warrior
-    rageMax: u16,
+    rage: u8,      // Warrior
+    rage_max: u8,
     blocking: u8,
 
     power: u16,     // Hunter
-    powerMax: u16,
-    dodging: u8,
+    power_max: u16,
     dexterity: u16,
+    dodging: u8,
+}
+
+pub fn warriorBaseStats() {
+    let warriorBaseStats = baseStats {
+        hp: 100,
+        hp_max: 100,
+        strength: 10,
+        armor: 10,
+        rage: 0,
+        rage_max: 100,
+        blocking: 0,
+    };
+}
+
+pub fn mageBaseStats() {
+    let mageBaseStats = baseStats {
+        hp: 100,
+        hp_max: 100,
+        strength: 10,
+        armor: 10,
+        mana: 100,
+        mana_max: 100,
+        intelligence: 10,
+        dodging: 0,
+    };
+}
+
+pub fn hunterBaseStats() {
+    let hunterBaseStats = baseStats {
+        hp: 100,
+        hp_max: 100,
+        strength: 10,
+        armor: 10,
+        power: 100,
+        power_max: 100,
+        dodging: 0,
+        dexterity: 10,
+    };
 }
